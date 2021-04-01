ac8721c407e9f945ff6028359022883902297983
Radiyah <rsabur58@gmail.com>
2021-04-01 12:00:34 -0400

    Fixed add clothing to closet with minor issue

--- app/controllers/closets_controller.rb
+++ app/controllers/closets_controller.rb
@@ -9,6 +9,16 @@ class ClosetsController < ApplicationController
         # @closet = Closet.find(params[:id])
     end
 
-    
+    def create
+        # byebug
+       @add_clothes = Closet.create(closet_params)
+       redirect_to closets_path(@current_user)
+    end
+
+    private
+
+    def closet_params
+        params.permit(:user_id, :clothing_id)
+    end
 
 end
